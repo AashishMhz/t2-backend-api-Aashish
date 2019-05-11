acef571fa3a88d6c96ac7d5187911a1099062d58
Aashish Maharjan <aashishmaharjan10@gmail.com>
2019-05-11 22:03:30 +0545

    User route created

--- app.js
+++ app.js
@@ -2,31 +2,12 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var mongoose = require("mongoose");
-var session = require("express-session");
-var fileStore = require("session-file-store")(session);
-var Authenticate = require("./authenticate");
-var passport = require('passport');
 
-// const url = "mongodb://localhost:27017/mydb";
-// const connect = mongoose.connect(url, {
-//   useNewUrlParser: true,
-//   useCreateIndex: true
-// });
-
-connect.then(
-  db => {
-    console.log("Connected to mongodb server");
-  },
-  err => {
-    console.log(err);
-  }
-);
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var productRouter = require("./routes/product");
-var product_typeRouter = require("./routes/product_type");
+const url = "mongodb://localhost:27017/mydb";
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
 
 var app = express();
 
@@ -49,20 +30,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-function auth(req, res, next) {
-  console.log(req.user);
-  if (!req.user) {
-    let err = new Error("You are not authenticated!");
-    err.status = 403;
-    return next(err);
-  } else {
-    next();
-  }
-}
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-// app.use(auth);
-app.use("/product", productRouter);
-app.use("/product_type", product_typeRouter);
+app.use(auth);
+
 module.exports = app;
