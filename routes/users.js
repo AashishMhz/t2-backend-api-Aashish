eb460c00bc0abca6955bca69f341da36d10ccda3
Aashish Maharjan <aashishmaharjan10@gmail.com>
2019-05-28 11:52:47 +0545

    get users

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,12 @@ var passport = require("passport");
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
+  User.find({}).then((users)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'application/json');
+    res.json(users);
+  },(err) => next(err))
+  .catch((err) => next(err));
 });
 
 router.post("/signup", (req, res, next) => {
